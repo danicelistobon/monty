56a187abc07c7b763a47483f709c511b7faf81cc
Liliana327 <894@holbertonschool.com>
2019-09-26 20:43:28 +0000

    updated header file

--- monty.h
+++ monty.h
@@ -1,6 +1,10 @@
 #ifndef _MONTY_
 #define _MONTY_
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
@@ -30,5 +34,16 @@ typedef struct instruction_s
 	char *opcode;
 	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
+
+int read(char *filename);
+void free_stack(stack_t **stack);
+void operations(char *tokens, stack_t **stack, unsigned int line_num);
+void op_push(stack_t **stack, unsigned int line_num);
+void op_pall(stack_t **stack, unsigned int line_num __attribute__((unused)));
+void op_pint(stack_t **stack, unsigned int line_num);
+void op_pop(stack_t **stack, unsigned int line_num);
+void op_swap(stack_t **stack, unsigned int line_num __attribute__((unused)));
+void op_add(stack_t **stack, unsigned int line_num);
+void op_nop(stack_t **stack, unsigned int line_num);
 
 #endif
