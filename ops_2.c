795daafed5ee3005bbf690e84a470442d2a91834
Liliana327 <894@holbertonschool.com>
2019-09-26 20:26:36 +0000

    created add function

---
+++ ops_2.c
@@ -0,0 +1,24 @@
+#include "monty.h"
+/**
+* op_add - adds the top two elements of the stack
+*
+* @stack: pointer to head
+*
+* @line_num: line where the instruction appears
+*
+* Return: no return
+*/
+void op_add(stack_t **stack, unsigned int line_num)
+{
+	int temp;
+
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		printf("L%u: can't add, stack too short\n", line_num);
+		exit(EXIT_FAILURE);
+	}
+
+	temp = (*stack)->n;
+	op_pop(stack, line_num);
+	(*stack)->n += temp;
+}
