de8cbf4ce86534941cc5bc7217976e52bd942465
Liliana327 <894@holbertonschool.com>
2019-09-27 01:06:44 +0000

    created div function

---
+++ ops_3.c
@@ -0,0 +1,33 @@
+#include "monty.h"
+/**
+* op_div - divides the top two elements of the stack
+*
+* @stack: pointer to head
+*
+* @line_num: line where the instruction appears
+*
+* Return: no return
+*/
+void op_div(stack_t **stack, unsigned int line_num)
+{
+	int temp;
+
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%u: can't div, stack too short\n", line_num);
+		error_free(*stack);
+		exit(EXIT_FAILURE);
+	}
+
+	temp = (*stack)->n;
+
+	if (temp == 0)
+	{
+		fprintf(stderr, "L%u: division by zero\n", line_num);
+		error_free(*stack);
+		exit(EXIT_FAILURE);
+	}
+	op_pop(stack, line_num);
+	(*stack)->n /= temp;
+}
+
