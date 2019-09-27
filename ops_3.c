9ab980b7d539b6bf515a6af4e5e688115790b153
Liliana327 <894@holbertonschool.com>
2019-09-27 01:11:33 +0000

    created mod function

--- ops_3.c
+++ ops_3.c
@@ -30,4 +30,34 @@ void op_div(stack_t **stack, unsigned int line_num)
 	op_pop(stack, line_num);
 	(*stack)->n /= temp;
 }
+/**
+* op_mod - computes the rest of the division of the top two elements stack
+*
+* @stack: pointer to head
+*
+* @line_num: line where the instruction appears
+*
+* Return: no return
+*/
+void op_mod(stack_t **stack, unsigned int line_num)
+{
+	int temp;
 
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%u: can't mod, stack too short\n", line_num);
+		error_free(*stack);
+		exit(EXIT_FAILURE);
+	}
+
+	temp = (*stack)->n;
+
+	if (temp == 0)
+	{
+		fprintf(stderr, "L%u: division by zero\n", line_num);
+		error_free(*stack);
+		exit(EXIT_FAILURE);
+	}
+	op_pop(stack, line_num);
+	(*stack)->n %= temp;
+}
